c33221b187bbbd39c271285fd880d2fe2f2b9dae
blame Nikola Slavchev <nslavchev48@gmail.com>
2023-04-01 10:34:59 +0300

    get better styling

--- task8.1-react-native-fundamentals/App.js
+++ task8.1-react-native-fundamentals/App.js
@@ -9,14 +9,40 @@ import CustomModal from './src/components/Modal';
 
 const Stack = createNativeStackNavigator();
 
+const headerOptions = {
+  title: 'Dice APP',
+  headerStyle: {
+    backgroundColor: 'black',
+  },
+  headerTintColor: 'white',
+  headerTitleStyle: {
+    fontWeight: '900',
+    color: 'white',
+    alignSelf: 'center',
+  },
+  headerTitleAlign: 'center',
+};
+
 export default function App() {
   return (
     <DiceProvider>
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Roll Dice" component={RollingDiceScreen} />
-          <Stack.Screen name="Modal" component={CustomModal} />
+          <Stack.Screen
+            name="Home"
+            component={HomeScreen}
+            options={headerOptions}
+          />
+          <Stack.Screen
+            name="Roll Dice"
+            component={RollingDiceScreen}
+            options={headerOptions}
+          />
+          <Stack.Screen
+            name="Modal"
+            component={CustomModal}
+            options={headerOptions}
+          />
         </Stack.Navigator>
       </NavigationContainer>
     </DiceProvider>
--- task8.1-react-native-fundamentals/src/components/Modal/index.js
+++ task8.1-react-native-fundamentals/src/components/Modal/index.js
@@ -3,6 +3,8 @@ import { Modal, StyleSheet, Text, Pressable, View } from 'react-native';
 import { useContext } from 'react';
 import { Context as DiceContext } from '../../context/DiceContext';
 
+import { AntDesign } from '@expo/vector-icons';
+
 const CustomModal = ({ isVisible, onClose }) => {
   const {
     state: { numDices, sides },
@@ -26,13 +28,17 @@ const CustomModal = ({ isVisible, onClose }) => {
               style={[styles.button, styles.buttonApply]}
               onPress={() => changeDicesNumber(1)}
             >
-              <Text style={styles.textStyle}>+</Text>
+              <Text style={styles.textStyle}>
+                <AntDesign name="plus" size={24} color="white" />
+              </Text>
             </Pressable>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => changeDicesNumber(-1)}
             >
-              <Text style={styles.textStyle}>-</Text>
+              <Text style={styles.textStyle}>
+                <AntDesign name="minus" size={24} color="white" />
+              </Text>
             </Pressable>
           </View>
 
@@ -43,18 +49,26 @@ const CustomModal = ({ isVisible, onClose }) => {
               style={[styles.button, styles.buttonApply]}
               onPress={() => changeDicesSides(1)}
             >
-              <Text style={styles.textStyle}>+</Text>
+              <Text style={styles.textStyle}>
+                <AntDesign name="plus" size={24} color="white" />
+              </Text>
             </Pressable>
             <Pressable
               style={[styles.button, styles.buttonClose]}
               onPress={() => changeDicesSides(-1)}
             >
-              <Text style={styles.textStyle}>-</Text>
+              <Text style={styles.textStyle}>
+                <AntDesign name="minus" size={24} color="white" />
+              </Text>
             </Pressable>
           </View>
 
           <Pressable
-            style={[styles.button, styles.buttonClose]}
+            style={[
+              styles.button,
+              styles.buttonClose,
+              { width: 200, marginTop: 20 },
+            ]}
             onPress={onClose}
           >
             <Text style={styles.textStyle}>Close</Text>
